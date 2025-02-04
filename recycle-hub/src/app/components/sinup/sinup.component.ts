75e973d46e9c68eb2daf345b28f66d1d9a36ca66
Walid Saifi <saifiwalid21@gmail.com>
2025-02-04 09:58:00 +0000

    ajouter composent login sinup

---
+++ recycle-hub/src/app/components/sinup/sinup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sinup',
+  standalone: true,
+  imports: [],
+  templateUrl: './sinup.component.html',
+  styleUrl: './sinup.component.scss'
+})
+export class SinupComponent {
+
+}
